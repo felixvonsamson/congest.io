f37b112897758a6857745935f1655b4d5aa1f589
felixvonsamson <felixvonsamson@gmail.com>
2026-02-24 20:09:56 +0100

    moving static into public folder

--- frontend/ui/level_selection.js
+++ frontend/ui/level_selection.js
@@ -1,7 +1,7 @@
 import { load_level } from "../main.js";
 
 function populateLevelList() {
-    // for the 37 pictures found in frontend/static/images/level miniatures, add div in the div called levelList that displays the image of the level anf the number on top.
+    // for the 37 pictures found in frontend/public/static/level miniatures, add div in the div called levelList that displays the image of the level anf the number on top.
     const levelListDiv = document.getElementById('levelList');
     const player = JSON.parse(sessionStorage.getItem('player'));
     for (let i = 1; i <= 37; i++) {
@@ -9,13 +9,13 @@ function populateLevelList() {
         levelDiv.className = 'levelItem';
         levelDiv.innerHTML = `
         <div class="levelItem">
-            <img src="./static/images/level_miniatures/level${i}.png" alt="Level ${i}" class="levelThumbnail">
+            <img src=".public/static/level_miniatures/level${i}.png" alt="Level ${i}" class="levelThumbnail">
             <div class="levelNumber">${i}</div>
         </div>
         `;
         if (i > player.unlocked_levels) {
             levelDiv.classList.add('lockedLevel');
-        }else {
+        } else {
             levelDiv.addEventListener('click', () => {
                 load_level(i);
                 document.getElementById('levelSelectionPanel').style.display = 'none';
@@ -28,7 +28,7 @@ function populateLevelList() {
 document.getElementById('levelSelection').addEventListener('click', (e) => {
     document.getElementById('levelSelectionPanel').style.display = 'block';
     document.getElementById('menuButtons').style.display = 'none';
-    if (document.getElementById('levelList').children.length === 0){
+    if (document.getElementById('levelList').children.length === 0) {
         populateLevelList();
     }
 });
